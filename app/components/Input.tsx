0fa628a9d2d47a053348c51d717974e7c99a0e0e
ILWEL ISAAC <ilwelisaac@hotmail.com>
2024-01-18 14:58:22 -0300

    feat: input animations

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,14 +1,47 @@
-import { InputHTMLAttributes } from "react";
+import { motion, spring } from "framer-motion";
+import { InputHTMLAttributes, useRef, useState } from "react";
+import { useOutside } from "../hooks/useOutside";
 
 interface InputInterface extends InputHTMLAttributes<HTMLInputElement>{
   label: string
 }
 
+const varContainer = {
+  init: { scale: 1},
+}
+
+const varLabel = {
+  init: {top: 10, left: 10, opacity: 0.5},
+  focus: { top: -25, left: 0, fontSize: "13px"}
+}
+
 export default function Input( props : InputInterface ){
+
+  const [onFocusTrigger, setOnFocusTrigger] = useState(false)
+  const ref = useRef(null)
+  const inputRef = useRef<HTMLInputElement>(null)
+  const verifyInput = () => {
+    if(inputRef.current !== null && !inputRef.current.value)
+    setOnFocusTrigger(false)
+  } 
+  useOutside(ref, verifyInput)
+
   return( 
-    <>
-      <label htmlFor={props.name}>{props.label}</label>
-      <input {...props} />
-    </>
+    <motion.div
+      ref={ref}
+      variants={varContainer}
+      initial='init'
+      animate= {onFocusTrigger ? 'focus' : 'init'} 
+      className="flex flex-col relative mt-4"
+      onClick={() => setOnFocusTrigger(true)}
+      onFocus={() => setOnFocusTrigger(true)}
+      onBlur={verifyInput}
+    >
+      <motion.label
+      className="absolute pointer-events-none"
+      variants={varLabel}
+       htmlFor={props.name}>{props.label}</motion.label>
+      <input ref={inputRef} {...props} />
+    </motion.div>
   )
 }
--- app/sign-in/components/Content.tsx
+++ app/sign-in/components/Content.tsx
@@ -6,15 +6,15 @@ import Link from "next/link";
 export default function Content (){
   return (
     <div className="sign-in flex flex-col gap-4">
-      <div className="card flex flex-col gap-10">
-        <h1>Login</h1>
+      <div className="card flex flex-col gap-6">
+        <h1>Sign In</h1>
         <div className="flex flex-col gap-4">
           <Input label="Username"/>
-          <Input label="Senha" type="password"/>
+          <Input label="Password" type="password"/>
         </div>
-        <Button delay={0.5}> Iniciar Sessão</Button>
+        <Button delay={0.5}> Sign In</Button>
       </div>
-      <Link href="/sign-out"> Não possuo uma conta</Link>
+      <Link href="/sign-out"> I Dont Have An Account</Link>
     </div>
   )
 }
