d3429645e7e91f0805039693558d6a66e712cfd6
ILWEL ISAAC <ilwelisaac@hotmail.com>
2024-01-22 15:24:26 -0300

    chore: redirect to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+'use client'
 import Button from './components/Button'
 import H1Anim from './components/H1Anim'
+import { useSession } from './utils/hooks/useSession'
 
 export default function Home() {
+  const { id } = useSession()
   return (
     <main className="home">
       <H1Anim texts={['Playing With Your Inheritance']} />
-      <Button href="/sign-in" delay={2}>
+      <Button href={id != null ? '/home' : '/sign-in'} delay={2}>
         Start
       </Button>
     </main>
