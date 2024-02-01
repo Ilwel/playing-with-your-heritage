6791cfa0eef8cca6666671205e870f94bb29139c
Aldebaran <elton.alde@gmail.com>
2024-02-01 11:44:05 -0300

    fix: multiples button requisitions

--- app/game/[id]/components/Hud.tsx
+++ app/game/[id]/components/Hud.tsx
@@ -1,52 +1,62 @@
 'use client'
-import { setGame, type GameState } from "@/app/utils/redux/features/gameSlice";
+import Button from "@/app/components/Button";
+import { useGame } from "@/app/utils/hooks/useGame";
+import { useSession } from "@/app/utils/hooks/useSession";
+import { setGame } from "@/app/utils/redux/features/gameSlice";
 import { type AppDispatch } from "@/app/utils/redux/store";
 import { Dice6 } from "lucide-react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 
-export default function Hud (props : {game : GameState, username : string}) {
-  
+export default function Hud () {
+
+  const { game }= useGame()
+  const { username} = useSession()
+  const { players, turnPlayer } = game
+  const currentPlayer = players[turnPlayer]
+  const [rollingDice, setRollingDice] = useState(false)
   const dispatch = useDispatch<AppDispatch>()
 
+const handleRollDice = () => {
+  if (!rollingDice) {
+    setRollingDice(true);
+    const rollDice = Math.floor(Math.random() * 6) + 1;
+    const updatedPlayers = players.map((player, index) => 
+      index === turnPlayer ? { ...player, money: player.money + rollDice } : player
+    )
+
+    dispatch(setGame({
+      ...game,
+      players: updatedPlayers,
+      turnPlayer: (turnPlayer + 1) % players.length
+    }));
+
+    setTimeout(() => { setRollingDice(false) }, 1000);
+  }
+}
 
   return (
-
-    <main>
-      <div className="absolute top-0 left-0">
-        {props.game?.players.map((player, index) => (
-          <div key={player.user.id} className="flex flex-col gap-2">
-            <div className="flex gap-2 border-2 rounded-lg m-2 p-2">
-              <span className="font-bold">{player.user.username}</span>
-              <span className="font-bold">{player.money}</span>
-              {player.user.username === props.username && <span className="font-bold text-green-300">( you )</span>}
-            </div>
+    <div className="p-2 m-2 rounded-lg pointer-events-auto">
+      {players.map((player, index) => (
+        <div key={index} className="flex items-center space-x-2 mb-2">
+          <div className="border-2 p-2 m-2 rounded-lg">
+            {player.user.username}
+            {player.user.username === username && <span className="font-bold text-green-300">( you )</span>}
+            {player.money}
           </div>
-        ))}
-      </div>
-      {props.game?.players[props.game?.turnPlayer]?.user.username === props.username && (
-        <div className="absolute bottom-0 right-0 ">
-          <button
-            className="m-2 p-2 rounded-lg"
-            onClick={() => {
-              const rolldice = Math.floor(Math.random() * 6) + 1
-              const actualPlayer = props.game.turnPlayer
-              const players = props.game.players.map(player => ({...player}))
-              players[actualPlayer].money += rolldice
-
-              dispatch(setGame({
-                ...props.game,
-                players,
-                turnPlayer: ((actualPlayer + 1)% (props.game.players.length))
-              }))
-              
-            }}
+        </div>
+      ))}
+      {currentPlayer?.user.username === username || !rollingDice && (
+        <div className="absolute bottom-0 right-0 m-4">
+          <Button
+            disabled={rollingDice}
+            className="font-bold py-2 px-4 rounded"
+            onClick={handleRollDice}
           >
             <Dice6 />
-          </button>
-
+          </Button>
         </div>
-        
       )}
-    </main>
+    </div>
   )
 }
