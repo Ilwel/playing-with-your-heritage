924758122d48a5988835975203c57de2b137359e
Aldebaran <elton.alde@gmail.com>
2024-02-01 11:42:08 -0300

    refactor: removing Hud from canvas

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -1,37 +1,38 @@
 'use client'
 import { Box } from '@/app/components/3d/Box'
-import { Html } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import { useGame } from '@/app/utils/hooks/useGame'
 import Hud from './components/Hud'
-import { useSession } from '@/app/utils/hooks/useSession'
-import { ReduxProvider } from '@/app/utils/redux/provider'
-
+import { useState } from 'react'
 export default function Game() {
-  const { game } = useGame()
-  const { username } = useSession()
+
+  const [color, setColor] = useState('red')
 
   return (
-    <Canvas camera={{ position: [4, -4, 4] }}>
-     
-      <Html
-        fullscreen
-      > 
-       <ReduxProvider>
-       <Hud game={game} username={username} />
-        </ReduxProvider>
-        
-      </Html>
-      <ambientLight intensity={Math.PI} />
-      <spotLight
-        position={[3, 3, 3]}
-        angle={-5}
-        penumbra={1}
-        decay={0}
-        intensity={Math.PI}
-      />
-      <pointLight position={[-50, -50, -50]} decay={0} intensity={Math.PI} />
-      <Box position={[0, 0, 0]} />
-    </Canvas>
+    <div>
+      <div className='relative w-screen h-screen'>
+        <Canvas camera={{ position: [4, -4, 4] }}>
+          <ambientLight intensity={Math.PI} />
+          <spotLight
+            position={[3, 3, 3]}
+            angle={-5}
+            penumbra={1}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <pointLight position={[-50, -50, -50]} decay={0} intensity={Math.PI} />
+          <Box
+            onClick={() => {
+            
+              setColor('blue')
+              
+            }}
+            color={color}
+          position={[0, 0, 0]} />
+        </Canvas>
+        <div className='absolute top-0 left-0 w-full h-full pointer-events-none'>
+          <Hud/>
+        </div>
+      </div>
+    </div>
   )
 }
