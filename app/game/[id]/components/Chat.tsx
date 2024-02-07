2c7654a382749cac876234681391c4b410c5a5c4
ILWEL ISAAC <ilwelisaac@hotmail.com>
2024-02-07 14:58:33 -0300

    feat: auto scroll chat on new messages

--- app/game/[id]/components/Chat.tsx
+++ app/game/[id]/components/Chat.tsx
@@ -4,26 +4,40 @@ import { useGame } from '@/app/utils/hooks/useGame'
 import { useSession } from '@/app/utils/hooks/useSession'
 import { AnimatePresence, motion } from 'framer-motion'
 import { MessageCircleMore, XOctagon } from 'lucide-react'
-import { type KeyboardEvent, useState } from 'react'
+import { type KeyboardEvent, useState, useRef, useEffect } from 'react'
 
 export default function Chat() {
   const { username } = useSession()
   const { handleChat, game } = useGame()
   const [open, setOpen] = useState(true)
+  const sentinelRef = useRef<HTMLDivElement>(null)
 
   const handleSendMessage = () => {
     const input = document.getElementById('msg-input') as HTMLInputElement
-    handleChat({
-      msg: input.value,
-      username,
-      createdAt: new Date().toLocaleTimeString(),
-    })
-    input.value = ''
+    if (input.value !== '') {
+      handleChat({
+        msg: input.value,
+        username,
+        createdAt: new Date().toLocaleTimeString(),
+      })
+      input.value = ''
+    }
   }
 
   const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
     if (e.key === 'Enter') {
       handleSendMessage()
+    }
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [game.chat])
+
+  const scrollToBottom = () => {
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (sentinelRef.current) {
+      sentinelRef.current.scrollIntoView({ behavior: 'smooth' })
     }
   }
 
@@ -52,8 +66,8 @@ export default function Chat() {
                   <XOctagon />
                 </div>
               </div>
-              <div className="flex flex-col justify-end p-3 border border-black h-[300px] max-h-[300px] min-w-[400px] max-w-[400px] overflow-scroll">
-                {game.chat.map((msg) => (
+              <div className="flex flex-col p-3 border border-black h-[300px] min-w-[400px] max-w-[400px] overflow-y-scroll">
+                {game.chat.map((msg, index) => (
                   <div
                     className="flex gap-1 text-sm"
                     key={`${msg.msg}-${msg.createdAt}-${msg.username}`}
@@ -63,9 +77,17 @@ export default function Chat() {
                     <p>{msg.msg}</p>
                   </div>
                 ))}
+                <div
+                  style={{ float: 'left', clear: 'both' }}
+                  ref={sentinelRef}
+                />
               </div>
-              <div className="flex gap-1">
-                <Input id="msg-input" label="Send A Message" />
+              <div className="flex gap-1 w-full">
+                <Input
+                  id="msg-input"
+                  className="w-full"
+                  label="Send A Message"
+                />
                 <Button
                   delay={0.3}
                   className="mt-4"
